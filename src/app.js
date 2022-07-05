9d62c759fc1bbb90c3d652fb125d703971eb0316
JOHNSON KHADKA <er.johnsonkhadka@gmail.com>
2022-07-06 00:14:35 +0545

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000
 
 // Define path for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -112,6 +113,6 @@ app.get("/*", (req, res) => {
 //     message:"Help not found."})
 // })
 
-app.listen(3000, () => {
-  console.log("Server is running...");
+app.listen(port, () => {
+  console.log("Server is running at " + port);
 });
