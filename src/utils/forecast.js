7d3c3f86303bcb17960ea4d84ae21a70daa3bd07
JOHNSON KHADKA <er.johnsonkhadka@gmail.com>
2022-07-08 09:14:00 +0545

    feelslike parameter added in weather forecastdata

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('postman-request')
 // const chalk = require('chalk')
 
 const forecast = (latitude,longitude,callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=31931a8cf4ce46f8854e839fc6dd3413&query='+latitude+','+longitude
+    const url = 'http://api.weatherstack.com/current?access_key=5b2dcd5e824e81d381669332f10b34fb&query='+latitude+','+longitude
     request({url,json:true}, (error,{body})=>{
         if(error){
             callback('Unable to connect to the weather service!',undefined)
@@ -11,7 +11,7 @@ const forecast = (latitude,longitude,callback) => {
         }
         else{
             
-            callback(undefined , body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degrees out. There is "+ body.current.precip+ " % chance of rain.")
+            callback(undefined , body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degrees out. There is "+ body.current.precip+ " % chance of rain."+ "Feels like "+body.current.feelslike +" degrees out there.")
         }
     })
 }
